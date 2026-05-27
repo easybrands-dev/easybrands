b3f42e5cd7f6f158fa8e4ce901e4aaed799bd5d4
neo <neo@neo2.tail730b7e.ts.net>
2026-05-28 09:47:36 +1000

    Fix sitemap and robots for static export

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,5 +1,7 @@
 import type { MetadataRoute } from 'next'
 
+export const dynamic = 'force-static'
+
 const base = 'https://easybrands.com.au'
 
 export default function sitemap(): MetadataRoute.Sitemap {
